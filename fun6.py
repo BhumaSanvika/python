101aec64a2ea2c0523021dfa878421960380d53c
BhumaSanvika <haisrisanvika@gmail.com>
2026-07-23 14:57:29 +0530

    Add files via upload

---
+++ fun6.py
@@ -0,0 +1,4 @@
+def disp_info(first_name,last_name):
+    print("first name:",first_name)
+    print("last name:",last_name)
+disp_info(last_name = 'Cartman', first_name = 'Eric')
---
+++ fun9.py
@@ -0,0 +1,9 @@
+
+message = 'Hello'
+
+def greet():
+    
+    print('Local', message)
+
+greet()
+print('Global', message)
